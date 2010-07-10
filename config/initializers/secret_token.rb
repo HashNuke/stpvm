7101a182f9f9774978b1b64f1f7bfbc730e1dff4
Akash Manohar J <akash@akash.im>
2010-07-10 09:49:49 +0530

    added gallery and some template stuff

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+Rails.application.config.secret_token = 'd64b8e4923477d18a489558cb0635911ad95099ca784ec0d23e44067eb9e9cf9629708cf6a3ae4fe1faba022a3574cdfaf22d02ab900e622ec51eff26b788991'
