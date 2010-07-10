5f7dd79268b98150028af9dc2b970e229e1e6fa1
Akash Manohar J <akash@akash.im>
2010-07-10 14:12:39 +0530

    rotation working in FF and chrome

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,16 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+
+$(document).ready(function() {
+          $("#railspic").rotate(-20);
+
+          $(".rimg").bind('mouseover',function(){
+                $(this).rotateAnimation(0);
+          });
+
+          $(".rimg").bind('mouseout',function(){
+                $(this).rotateAnimation(-20);
+          });
+
+        });
